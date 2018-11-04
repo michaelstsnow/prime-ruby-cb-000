05a1f2fd1fa743ba2753f9e4bc9334edbd485f72
michaelstsnow <michael.st.snow@gmail.com>
2018-11-04 14:51:30 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
 # Add  code here!
+def prime?(num)
+  prime_numers=[2,3,5,7]
+  pos_num=num.abs;
+  if prime_numers.include?(pos_num)
+    return true
+  elsif pos_num == 1 || pos_num == 0
+    return false
+  else
+    i = 2;
+    while i<(pos_num/2)+2
+      if pos_num % i == 0
+        return false
+      else
+      end
+      i +=1;
+    end
+    return true
+  end
+end
