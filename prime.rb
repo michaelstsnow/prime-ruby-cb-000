31c15ee7e03acbd87ccaa6c6b229d4aea7778bb4
michaelstsnow <michael.st.snow@gmail.com>
2018-11-04 14:48:06 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -7,11 +7,11 @@ def prime?(num)
     i = 2;
     while i<num/2
       if num % i == 0
-        return true
+        return false
       else
       end
       i +=1;
     end
-    return false
+    return true
   end
 end
