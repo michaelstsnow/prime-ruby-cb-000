457ec398fddc57e7d9700b36b559d9d425144e85
michaelstsnow <michael.st.snow@gmail.com>
2018-11-04 14:45:04 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,17 +1,4 @@
 # Add  code here!
 def prime?(num)
-  i=1;
-  limit = i+1;
-  while i < limit
-    if num % i == 0
-      return false
-    else
-      i += 1
-      if i < 10
-        limit = i+1;
-      else
-        limit = num/2;
-      end
-    end
-  end
+  prime_numers=[1,2,3,5,7]
 end
